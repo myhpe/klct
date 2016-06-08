16c03c51ffa8196257e0055d19bb8755250205d2
andy <andylou2@illinois.edu>
2016-06-08 16:48:26 -0700

    fixed compile error

--- klct/ldap/configTool.py
+++ klct/ldap/configTool.py
@@ -4,39 +4,39 @@ import ldap3
 from ldap3 import Server, Connection, ALL
 
 
-def checkValidIP(hostName):
+def check_valid_IP(host_name):
     """Checks if the given hostName is a valid IP address.
     Return 1 if valid, 0 if invalid
     note: only checks for valid ipv4 IPs
           need to implement validation for URLs and IPV6
     """
     try:
-        socket.inet_aton(hostName)
+        socket.inet_aton(host_name)
         return 1
     except socket.error:
         return 0
 
 
-def pingLDAPserver(hostName):
+def ping_LDAP_server(host_name):
     """Checks if the given hostName is valid, and pings it
     Returns a string message indicating success of failure
     """
-    isValid = checkValidIP(hostName)
-    if not isValid:
-        return "Invalid Hostname"
+    is_valid = check_valid_IP(host_name)
+    if not is_valid:
+        return "Invalid Hostname Format"
 
-    print("ping -c 1" + hostName)
-    response = os.system("ping -c 1 " + hostName)
+    print("ping -c 1" + host_name)
+    response = os.system("ping -c 1 " + host_name)
     print("response: %d", response)
     if response == 0:
-        return "Successfully pinged " + hostName
+        return "Successfully pinged " + host_name
     else:
-        return "Unsuccessfully pinged " + hostName
+        return "Unsuccessfully pinged " + host_name
 
 
-def connectLDAPserver(hostName, userName, password):
+def connect_LDAP_server(host_name, user_name, password):
     try:
-        server = Server(hostName, get_info=ALL)
+        server = Server(host_name, get_info=ALL)
         print("\nTrying to connect...")
         #conn = Connection(server, authentication = ldap3.AUTH_SIMPLE, user=userName, password=password)
         conn = Connection(server)
@@ -56,5 +56,6 @@ def connectLDAPserver(hostName, userName, password):
         raise
         return "Failed to connect"
 
-def retrieveServerInfo(hostName, userName, password)
-    connectLDAPserver(hostname, userName, password)
+def retrieve_server_info(host_name, user_name, password):
+    print("temp")
+#ad vs. openldap, version, etc
