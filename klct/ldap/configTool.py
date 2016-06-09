9ef7f81307ab11472a53e656600ee367992911cb
andy <andylou2@illinois.edu>
2016-06-09 10:55:08 -0700

    added username/password authentication to task 2, need to add tls certificate checking

--- klct/ldap/configTool.py
+++ klct/ldap/configTool.py
@@ -1,12 +1,13 @@
 import os
 import socket
+import sys
 import ldap3
 from ldap3 import Server, Connection, ALL
 
 
 def check_valid_IP(host_name):
     """Checks if the given hostName is a valid IP address.
-    Return 1 if valid, 0 if invalid
+    Return 1 if valid, 0 if invalid.
     note: only checks for valid ipv4 IPs
           need to implement validation for URLs and IPV6
     """
@@ -18,43 +19,41 @@ def check_valid_IP(host_name):
 
 
 def ping_LDAP_server(host_name):
-    """Checks if the given hostName is valid, and pings it
-    Returns a string message indicating success of failure
+    """Checks if the given hostName is valid, and pings it.
+    Returns a string message indicating success of failure.
     """
     is_valid = check_valid_IP(host_name)
     if not is_valid:
         return "Invalid Hostname Format"
-
-    print("ping -c 1" + host_name)
     response = os.system("ping -c 1 " + host_name)
-    print("response: %d", response)
     if response == 0:
         return "Successfully pinged " + host_name
     else:
         return "Unsuccessfully pinged " + host_name
 
 
-def connect_LDAP_server(host_name, user_name, password):
+def connect_LDAP_server(host_name, port_number, user_name, password):
+    """Attempts to connect to the provided hostName, using the provided user name and pass.
+    Return a string indicating the success or failure (along with failure reasons) of the connection. 
+    Return an exception indicating what went wrong (returns None on success).
+    """
     try:
-        server = Server(host_name, get_info=ALL)
+        server = Server(host_name, port=port_number, get_info=ALL, use_ssl=True)
         print("\nTrying to connect...")
-        #conn = Connection(server, authentication = ldap3.AUTH_SIMPLE, user=userName, password=password)
-        conn = Connection(server)
+        conn = Connection(server, user=user_name, password=password)
         if not conn.bind():
-            print("bind failed: ", conn.result)
-            return conn.result
+            return"bind failed", conn.results
         else:
             conn.unbind()
-            return "Successfully connected!"
+            return "Successfully connected!", None
     except ldap3.LDAPSocketOpenError as err:
-        print("failed to connect")
-        print("error: ", err)
-        print("error is of type: ", type(err))
-        raise
-        return err
+        return "Failed to connect due to invalid socket.", err
+    except AttributeError as err:
+        return "Invalid log in info", err
+    except ldap3.LDAPPasswordIsMandatoryError as err:
+        return "Please enter a password.", err
     except:
-        raise
-        return "Failed to connect"
+        return "Failed to connect due to unknown reasons.", sys.exc_info()[0]
 
 def retrieve_server_info(host_name, user_name, password):
     print("temp")
--- klct/ldap/unitTests.py
+++ klct/ldap/unitTests.py
@@ -16,51 +16,51 @@ class TestConfigTool(unittest.TestCase):
 
     """pingLDAPserver() tests"""
     def test_ping_invalid_host_name(self):
-        self.assertEqual(configTool.pingLDAPserver(self.invalid_host_name), "Invalid Hostname")
+        self.assertEqual(configTool.ping_LDAP_server(self.invalid_host_name), "Invalid Hostname Format")
 
     def test_ping_good_host_name(self):
-        self.assertEqual(configTool.pingLDAPserver(self.good_host_name), "Successfully pinged " + self.good_host_name)
+        self.assertEqual(configTool.ping_LDAP_server(self.good_host_name), "Successfully pinged " + self.good_host_name)
 
     def test_ping_bad_host_name(self):
-        self.assertEqual(configTool.pingLDAPserver(self.bad_host_name), "Unsuccessfully pinged " + self.bad_host_name)
+        self.assertEqual(configTool.ping_LDAP_server(self.bad_host_name), "Unsuccessfully pinged " + self.bad_host_name)
 
     def test_ping_bad_host(self):
-        self.assertEqual(configTool.pingLDAPserver(self.bad_host), "Unsuccessfully pinged " + self.bad_host)
+        self.assertEqual(configTool.ping_LDAP_server(self.bad_host), "Unsuccessfully pinged " + self.bad_host)
 
     def test_ping_no_network(self):
         #this test should fail because it tests the local host
         call(["sudo", "ip", "link", "set", "dev", "eth0", "down"])
-        self.assertEqual(configTool.pingLDAPserver(self.good_host_name), "Unsuccessfully pinged " + self.good_host_name)
+        self.assertEqual(configTool.ping_LDAP_server(self.good_host_name), "Unsuccessfully pinged " + self.good_host_name)
         call(["sudo", "ip", "link", "set", "dev", "eth0", "up"])
 
     """connectLDAPserver() tests"""
     def test_connect_invalid_host_name(self):
-        self.assertRaises(ldap3.LDAPSocketOpenError, configTool.connectLDAPserver, self.invalid_host_name, self.user_name, self.password)
+        self.assertEqal(configTool.connect_LDAP_server(self.invalid_host_name, self.user_name, self.password), "Failed to connect due to invalid socket.")
 
     def test_connect_bad_host_name(self):
-        self.assertRaises(ldap3.LDAPSocketOpenError, configTool.connectLDAPserver, self.bad_host_name, self.user_name, self.password)
+        self.assertEqual(configTool.connect_LDAP_server(self.bad_host_name, self.user_name, self.password), "Failed to connect due to invalid socket.")
 
     def test_connect_bad_host(self):
-        self.assertRaises(ldap3.LDAPSocketOpenError, configTool.connectLDAPserver, self.bad_host, self.user_name, self.password)
+        self.assertEqual(configTool.connect_LDAP_server(self.bad_host, self.user_name, self.password), "Failed to connect due to invalid socket.")
 
     def test_connect_no_network(self):
         #this test should fail because it tests the local host
         call(["sudo", "ip", "link", "set", "dev", "eth0", "down"])
-        self.assertRaises(ldap3.LDAPSocketOpenError, configTool.connectLDAPserver, self.good_host_name, self.user_name, self.password)
+        self.assertEqual(configTool.connect_LDAP_server(self.good_host_name, self.user_name, self.password), "Failed to connect due to invalid socket.")
         call(["sudo", "ip", "link", "set", "dev", "eth0", "up"])
 
     #def test_connect_bad_port(self):
 
     def test_connect_good_host_name(self):
-        self.assertEqual(configTool.connectLDAPserver(self.good_host_name, self.user_name, self.password), "Successfully connected!")
+        self.assertEqual(configTool.connect_LDAP_server("10.1.56.3", "", ""), "Successfully connected!")
 
     def test_bad_username(self):
         print("testing bad username")
-        configTool.connectLDAPserver(self.good_host_name, self.bad_user_name, self.password)
+        self.assertEqual(configTool.connect_LDAP_server("10.1.56.3", self.bad_user_name, self.password), "Invalid log in info")
 
     def test_bad_password(self):
         print("testing bad password")
-        configTool.connectLDAPserver(self.good_host_name, self.user_name, self.bad_password)
+        configTool.connect_LDAP_server(self.good_host_name, self.user_name, self.bad_password)
 
 if __name__ == '__main__':
     unittest.main()
